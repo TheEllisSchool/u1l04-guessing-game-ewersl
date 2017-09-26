90c935ec55627b87e0e853a7d0611d9efaaab7df
leahewers <leahewers@172.16.20.189>
2017-09-26 11:09:36 -0400

    this checks to make sure you entered a number between zero and 100 over and over again

--- src/U1L04GuessingGame.java
+++ src/U1L04GuessingGame.java
@@ -1,10 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class U1L04GuessingGame {
 
 	public static void main(String[] args) {
 		int computerNum = (int)(Math.random() * 100);
-		System.out.println(computerNum);
+		int userNumber; 
+	do {
+		String input = JOptionPane.showInputDialog(null, "Enter your guess from 1 to 100 (if you give up, enter -1) ");
+		userNumber = Integer.parseInt(input);
+	} while (userNumber < 100 || userNumber > 0); 
 
+	
 	}
 
 }
