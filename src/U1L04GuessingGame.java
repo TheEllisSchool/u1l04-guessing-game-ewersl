17a07b5d7b68bd5d56b4e78000da67abfd9b5fd5
leahewers <leahewers@172.16.20.189>
2017-10-03 09:03:57 -0400

    guessing game final

--- src/U1L04GuessingGame.java
+++ src/U1L04GuessingGame.java
@@ -4,13 +4,30 @@ public class U1L04GuessingGame {
 
 	public static void main(String[] args) {
 		int computerNum = (int)(Math.random() * 100);
-		int userNumber; 
+		int userNumber = -1; 
+		
+while (userNumber != computerNum) {
 	do {
 		String input = JOptionPane.showInputDialog(null, "Enter your guess from 1 to 100 (if you give up, enter -1) ");
 		userNumber = Integer.parseInt(input);
-	} while (userNumber < 100 || userNumber > 0); 
-
+	} while (userNumber > 100 || userNumber < 0);
+		if (userNumber < computerNum - 10) { 
+			JOptionPane.showMessageDialog (null, "Too low!" );
+		}
+		if (userNumber > computerNum + 10) {
+			JOptionPane.showMessageDialog (null, "Too high!");
+		}
+		if (userNumber < computerNum + 10 && userNumber > computerNum + 10) {
+			JOptionPane.showMessageDialog (null, "You're within 10");
+		}
+		if (userNumber == computerNum) {
+			JOptionPane.showMessageDialog (null, "CORRRRREEEEECCCCCCTTTTT! congrats. ");
+		}
+		
 	
+	
+
+}
 	}
 
 }
